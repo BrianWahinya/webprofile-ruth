c52b70fb01d9651ea4626a9c18f593f92d90f3dd
bngure <brianwahinyangure@gmail.com>
2024-08-15 15:22:05 +0300

    Love-mocha gif added to the main to load from the onset

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/main.css";
 
 import music_mp3 from "./assets/love-music.mp3";
+import love_mocha from "./assets/love-mocha-bear-no-bg.gif";
 
 const audio = new Audio(music_mp3);
 audio.pause();
 audio.currentTime = 0;
+
+const img = new Image();
+img.src = love_mocha;
 
 setTimeout(
   () =>
